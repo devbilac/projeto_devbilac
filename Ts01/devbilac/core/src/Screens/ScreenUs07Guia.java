0e020f52461872d05cca93591848d8be78b1293b
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2016-09-28 20:46:16 -0300

    Base
    Adicionado os arquivos Bases

---
+++ Ts01/devbilac/core/src/Screens/ScreenUs07Guia.java
@@ -0,0 +1,6 @@
+package Screens;
+//Classe Destinada a Exibição da Estrutura para o Usuario.
+//Esta Classe é parecida com as telas do Hud.
+public class ScreenUs07Guia {
+
+}
---
+++ Ts01/devbilac/core/src/Screens/ScreenUs07Jogo.java
@@ -0,0 +1,6 @@
+package Screens;
+
+//Tela destinada ao Jogo da User Story 07.
+public class ScreenUs07Jogo {
+
+}
