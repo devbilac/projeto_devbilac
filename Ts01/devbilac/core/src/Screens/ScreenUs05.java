438309088888480247c9c1eda89027a1ac7f9f65
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2016-10-16 17:47:22 -0200

    Adicionado Base para US05

---
+++ Ts01/devbilac/core/src/Screens/ScreenUs05.java
@@ -0,0 +1,97 @@
+package Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.DevBilac;
+
+import Scenes.HudUs05;
+import Scenes.HudUs07;
+
+public class ScreenUs05 implements Screen {
+	private HudUs05 hud;
+	//Variaveis Box2d
+	private World world;
+	private DevBilac game;
+	private OrthographicCamera gamecam;
+	private Viewport gamePort;
+	private SpriteBatch batch;
+	
+	public ScreenUs05(DevBilac game){
+		batch = new SpriteBatch();
+		// Chama o HudRanking que é um 'Display' na tela, onde ficas Labels.
+		this.game = game;
+		gamecam = new OrthographicCamera();
+		world = new World(new Vector2(0,-200 / DevBilac.PPM), true);
+		//Alterar para mudar a Gravidade do mundo
+		world = new World(new Vector2(0,-200 / DevBilac.PPM), true);
+		//o 'Metodo' FitViewport faz o redimencionamento de tela.
+		gamePort = new FitViewport(DevBilac.V_WIDHT / DevBilac.PPM,DevBilac.V_HEIGHT / DevBilac.PPM,gamecam);
+		gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() /2,0);
+		hud = new HudUs05(game.batch); 
+		hud.setTimer(60); //Envia o Tempo que o jogo tera para o Display.
+	}
+	
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void handleInput(float delta){
+		
+	}
+	
+	public void update(float delta){
+		handleInput(delta);
+		hud.update(delta);
+	}
+
+	@Override
+	public void render(float delta) {
+		update(delta);
+		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		hud.stage.draw(); 
+		batch.begin();
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		gamePort.update(width, height);
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
