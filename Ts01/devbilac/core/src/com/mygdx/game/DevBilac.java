25cd1e7f473182990acbfda1a1279032ebb9ad2d
Luiz Otávio Rodrigues <luiz.otavior18@gmail.com>
2016-11-24 11:10:00 -0200

    Implementação de ensino teorico.

--- Ts01/devbilac/core/src/com/mygdx/game/DevBilac.java
+++ Ts01/devbilac/core/src/com/mygdx/game/DevBilac.java
@@ -2,22 +2,21 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import Screens.PlayScreen;
-import Screens.ScreenUs05;
-import Screens.ScreenUs07;
+import Screens.ScreenUs03;
 
 public class DevBilac extends Game {
 	public static final int V_WIDHT = 400;
 	public static final int V_HEIGHT = 208;
-	public static final float PPM = 1;
+	public static final float PPM = 100;
+	public static final float PPM2 = 100;
 	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
+		//setScreen(new PlayScreen(this));
 		//setScreen(new Ranking(this));
-		//setScreen(new ScreenUs03(this));
+		setScreen(new ScreenUs03(this));
 		//setScreen(new ScreenUs07(this));
 		//setScreen(new ScreenUs05(this));
 		
